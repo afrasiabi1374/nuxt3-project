fd85a585d0e64725b812e74e4fb23078ee280f15
afrasiabi1374 <mohammad.aligiga@gmail.com>
2022-10-23 08:09:59 +0330

    a

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
+import { defineNuxtConfig } from 'nuxt'
+
 export default defineNuxtConfig({
     css: ["@/assets/css/tailwind.css"],
     build: {
       postcss: {
         postcssOptions: require("./postcss.config.js"),
       },
+      transpile: ['gsap']
     },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,38 @@
 <template>
     <div>
-        <h1>pages</h1>
+        <div class="box w-4 h-4 bg-red-400">1</div>
+        <div class="box w-4 h-4 bg-green-400">2</div>
+        <br><br>
+        field :<Form v-slot="{errors}">
+            <Field name="field" :rules="isRequired" />
+            {{errors}}
+        </Form>
     </div>
 </template>
-<script>
 
+<script lang="ts">
+import { Form, Field, ErrorMessage } from 'vee-validate'
+import { gsap } from 'gsap'
+export default defineComponent({
+    component: {
+        Form,
+        Field,
+        ErrorMessage 
+    },
+    methods: {
+        isRequired (value) {
+            return value ? true : 'this field is required'
+        }
+    },
+    setup () {
+        onMounted(()=>{
+            gsap.to('.box', {x: 100, duration: 1 })
+        })
+
+        return {}
+    }
+})
 </script>
+
+<style scoped>
+</style>
