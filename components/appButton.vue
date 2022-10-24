61eb7da456b1ad0e36fbacef0c6474c50bab8142
afrasiabi1374 <mohammad.aligiga@gmail.com>
2022-10-24 07:55:52 +0330

    btn

---
+++ components/appButton.vue
@@ -0,0 +1,30 @@
+<template>
+    <button :class="[`btn btn-${variant}`, { loading: loading }]">
+        <slot></slot>
+    </button>
+</template>
+
+<script setup lang="ts">
+    // const props = defineProps({
+    //     variant: {
+    //         type: String,
+    //         default: 'primary'
+    //     },
+    //     loading: {
+    //         type: Boolean,
+    //         default: false
+    //     }
+    // })
+    interface Props {
+        variant?:string;
+        loading?:boolean;
+    }
+    const props=withDefaults(defineProps<Props>(), {
+        variant: "primary",
+        loading: false
+    })
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/base.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex flex-col space-y-3 justify-center items-center">
+        <h1>my base components</h1>
+        <AppButton variant="secondary" >دکمه</AppButton>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped>
+
+</style> 
